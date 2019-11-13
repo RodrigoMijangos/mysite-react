61578eef6ffbf7d0bfa260f7fcf2de6bbda95a1e
Mijangos <183438@ids.upchiapas.edu.mx>
2019-11-13 00:50:07 -0600

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar(){
+    return(
+        <div className="navBar">
+            <h1>NavBar</h1>
+        </div>
+
+    );
+}
+
+export default NavBar;
